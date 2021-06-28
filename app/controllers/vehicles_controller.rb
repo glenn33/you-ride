2c409ec264ecf6badef57ab3bab874fd6d66443f
Glenn Gunter <gunterglenn33@gmail.com>
2021-06-28 23:42:29 +0200

    finished vehicles controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,22 +1,48 @@
 class VehiclesController < ApplicationController
-  def index
+ before_action :find, only: [:show, :edit, :update, :destroy]
+ before_action :authenticate_user!, only: :new
+ def index
+   @vehicles = Vehicle.all
+  end
+
+  def show
+   @booking = Booking.new
   end
 
   def new
+   @vehicle = Vehicle.new
   end
 
   def create
+   @vehicle = Vehicle.new(vehicle_params)
+   if @vehicle.save
+    redirect_to vehicle_path(@vehicle)
+   else
+    render :new
+   end
   end
 
   def update
+   @vehicle.update(vehicle_params)
+   redirect_to vehicle_path(@vehicle)
   end
 
-  def detroy
+  def destroy
+   @vehicle.destroy
+   redirect_to vehicles_path
   end
 
   def edit
   end
 
-  def show
+  private
+
+  def find
+   @vehicle = Vehicle.find(params[:id])
   end
+
+  def vehicle_params
+   params.require(:vehicle).permit(:category)
+  end
+
 end
