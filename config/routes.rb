25f5b0092cf5af3130d59c42fc88f9f0e96eae2f
santiago altamira <santiagoaltamira@gmail.com>
2021-06-29 18:36:16 -0300

    arreglando cosas de glenn

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
+
+# Pseudo coding
+# get "pages/show", to: "pages#show"
+# patch "pages/show/:id", to: "pages#update"
