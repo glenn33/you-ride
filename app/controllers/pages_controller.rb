f89d250e6ec06d4e868c46cab8f625ff28ea599c
Glenn Gunter <gunterglenn33@gmail.com>
2021-06-29 22:59:17 +0200

    almast finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,4 @@ class PagesController < ApplicationController
   def home
    @vehicles = Vehicle.new
   end
-
-  def account
-   @info = current_user
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/rentals', to: 'bookings#rentals'
   get '/my_bookings', to: 'bookings#index'
-  get '/account', to: 'pages#account'
 end
