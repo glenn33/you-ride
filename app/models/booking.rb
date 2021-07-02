1b195dd8668d72802538cfcc9a2a230656006ba4
santiago altamira <santiagoaltamira@gmail.com>
2021-07-02 13:23:32 -0300

    casi no hubo cambios

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,11 +2,18 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
 
-  validates :date, presence: true #, if: :smaller_than_end_date?
+  validates :date, presence: true 
   validates :end_date, presence: true
+  validate :end_date_after_start_date
+  
+ private
 
-  # def smaller_than_end_date?
-  #   @date < @end_date
-  # end
+#    def smaller_than_end_date?
+#      :date < :end_date
+#    end
+# end
+
+  def end_date_after_start_date
+      errors.add(:end_date, "can't be before your start date!") if end_date <= date
+  end
 end
-
